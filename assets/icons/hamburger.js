e21e5574392f059312e899222623a6051118d236
sohee28 <amybae28@gmail.com>
2023-01-24 23:56:34 -0900

    Added navbar menu icon

---
+++ assets/icons/hamburger.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hamburger = () => {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M3 12H21M3 6H21M3 18H21"
+        stroke="currentColor"
+        stroke-width="currentStroke"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default Hamburger;
--- src/layout/index.js
+++ src/layout/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Footer } from "../components/footer";
+import { Menu } from "../components/menu";
 import { Navbar } from "../components/navbar";
 import Theme from "../components/theme";
 
@@ -11,7 +12,11 @@ export const Layout = ({ children }) => {
       <div className="container">
         <Navbar />
         <div className="content">
-          <Theme />
+          <div className="header">
+            <Theme />
+            <Menu />
+          </div>
+
           {children}
         </div>
         <Footer />
