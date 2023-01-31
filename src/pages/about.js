e85ea0acf523c4cbf2f6dc188eef986c257f975d
sohee28 <amybae28@gmail.com>
2023-01-30 22:39:52 -0900

    added about page

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import "./about.scss";
+
+export default function About() {
+  return (
+    <div className="aboutRoot">
+      <div className="aboutNavbar" />
+      <div className="aboutContent">
+        <div className="aboutTitle">Your Name</div>
+        <hr />
+      </div>
+    </div>
+  );
+}
