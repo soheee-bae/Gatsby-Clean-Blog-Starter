aed7b5e2792c54a43243e35be7e52a2f6a4bfa83
sohee28 <amybae28@gmail.com>
2023-01-31 23:57:30 -0900

    Removed recursive navlist

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -6,35 +6,7 @@ import { CATEGORY, navigation } from "../../constants";
 export const Navbar = ({ handleSelect, selectedCategory }) => {
   const [show, setShow] = useState(CATEGORY.ALL);
 
-  const handleClick = (e, link) => {
-    setShow(link);
-    e.preventDefault();
-    e.stopPropagation();
-    handleSelect(link.toLowerCase());
-  };
 
-  const RecursiveNav = ({ data }) => {
-    return (
-      <div className="navList">
-        {data.map((parent) => {
-          const isShow = parent.link === show;
-          return (
-            <div
-              className="navDetail"
-              onClick={(e) => handleClick(e, parent.link)}
-            >
-              <div className="navTitle">
-                <p data-show={isShow}>{parent.title}</p>
-              </div>
-              {parent.children.length > 0 && (
-                <RecursiveNav data={parent.children} />
-              )}
-            </div>
-          );
-        })}
-      </div>
-    );
-  };
 
   return (
     <StaticQuery
@@ -52,9 +24,8 @@ export const Navbar = ({ handleSelect, selectedCategory }) => {
                 Github
               </Link>
             </div>
-            <div className="navLists">
-              <RecursiveNav data={navigation} />
-            </div>
+            <hr />
+        
           </div>
         );
       }}
